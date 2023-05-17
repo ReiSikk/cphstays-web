3d8269b61087b8b65cd4c7cd1f223e6a68886778
Rei Sikk <reix0010@stud.kea.dk>
2023-05-17 12:28:24 +0200

    landing hero section CTA done

--- app/components/BurgerMenu.jsx
+++ app/components/BurgerMenu.jsx
@@ -17,6 +17,7 @@ function BurgerMenu() {
 
 
 
+
   return (
     <>
     <nav id='burgerMenu'>
--- app/page.js
+++ app/page.js
@@ -1,45 +1,25 @@
+"use client"
 import Image from 'next/image'
 import Footer from './components/Footer'
 import NavigationBar from './components/NavigationBar'
+import SelectAutoWidth from './components/SelectAutoWidth'
+import BasicTextFields from './components/BasicTextFields'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
     <NavigationBar />
     <main className={styles.main}>
-      {/* I'm doing a hero section with a background image that has a small filtering window on it and also a list of 3 value propositions */}
       <section>
         <h1>Find a home away from home</h1>
         <div className='hero-container'>
           <Image src='/images/hero-img.webp' alt='hero image' width={1200} height={800} className="hero-img" />
-          <div className='filtering'>
+          <div className='hero-cta'>
             <h3>Find your perfect apartment and send a request</h3>
-            <div className='hero-filter-container'>
-            <div className='filtering-inputs'>
-              <label htmlFor='location'>Location
-            <select name="location" id="location" className="location">
-             <option value="all" defaultValue>
-              All locations
-            </option>
-             <option value="Østerbro">Østerbro</option>
-             <option value="Nørrebro">Nørrebro</option>
-             <option value="Vesterbro">Vesterbro</option>
-             <option value="IndreBy">Indre By</option>
-             <option value="Amager">Amager</option>
-             <option value="Valby">Valby</option>
-             <option value="Nordvest">Nordvest</option>
-             <option value="Bispebjerg">Bispebjerg</option>
-             <option value="Vanløse">Vanløse</option>
-             <option value="Vanløse">Brønshøj-Husum</option>
-            </select>
-              </label>
-              <label htmlFor='price'>Price</label>
-            <input type="text" placeholder="Enter rent" name="price" id="price" className="price "></input>
-            </div>
-            <button className="search-btn">
-              <Image src='/images/search-icon.svg' alt='search icon' width={24} height={24} className="search-img" />
-            </button>
+            <div className='hero-cta-container'>
+              <Link href='/apartments' className='secondary-btn'>See the apartments</Link>
             </div>
           </div>
         </div>
