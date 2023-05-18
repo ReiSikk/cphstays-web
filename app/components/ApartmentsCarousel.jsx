380fd1cba04ac025a34fbb35df41323734d9b33d
Rei Sikk <reix0010@stud.kea.dk>
2023-05-18 19:35:23 +0200

    needs more work on the mobile view

--- app/components/ApartmentsCarousel.jsx
+++ app/components/ApartmentsCarousel.jsx
@@ -5,15 +5,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { Pagination, Navigation } from "swiper";
 
 
 function ApartmentsCarousel() {
   return (
     <>
-      <Swiper
+    
+    <Swiper
         slidesPerView={1}
         spaceBetween={0}
         loop={true}
@@ -21,21 +22,25 @@ function ApartmentsCarousel() {
           clickable: true,
         }}
         breakpoints={{
-          640: {
+          540: {
             slidesPerView: 2,
-            spaceBetween: 20,
+            spaceBetween: 10,
           },
           768: {
             slidesPerView: 3,
-            spaceBetween: 40,
+            spaceBetween: 20,
           },
           1024: {
             slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1440: {
+            slidesPerView: 5,
             spaceBetween: 50,
           },
         }}
         navigation={true}
-        modules={[Pagination, Navigation]}
+        modules={[ Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
@@ -50,7 +55,19 @@ function ApartmentsCarousel() {
         <SwiperSlide>
             <ApartmentCard />
         </SwiperSlide>
-      </Swiper>
+        <SwiperSlide>
+            <ApartmentCard />
+        </SwiperSlide>
+        <SwiperSlide>
+            <ApartmentCard />
+        </SwiperSlide>
+        <SwiperSlide>
+            <ApartmentCard />
+        </SwiperSlide>
+        <SwiperSlide>
+            <ApartmentCard />
+        </SwiperSlide>
+      </Swiper> 
   </>
   )
 }
