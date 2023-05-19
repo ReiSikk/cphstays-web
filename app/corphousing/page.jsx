5f2eafa3776b37d269dcf7c125aef82bcfc50b4e
Rei Sikk <reix0010@stud.kea.dk>
2023-05-19 15:56:27 +0200

    contat

--- app/corphousing/page.jsx
+++ app/corphousing/page.jsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import HDIWsection from '../components/HDIWsection'
 import Carousel from '../components/ApartmentsCarousel'
 import Testimonials from '../components/Testimonials'
+import ContactForm from '../components/ContactForm'
 
 function page() {
   return (
@@ -45,6 +46,7 @@ function page() {
       <HDIWsection />
       <Carousel />
       <Testimonials /> 
+      <ContactForm />
 
     </>
   )
