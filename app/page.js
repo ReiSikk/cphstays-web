bb65a8842c1fcbbe45db3e1d04f621997c5ecd7d
Rei Sikk <reix0010@stud.kea.dk>
2023-05-20 14:07:50 +0200

    hero section almost done

--- app/page.js
+++ app/page.js
@@ -10,7 +10,33 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
+    <header className={styles.header}>
     <NavigationBar />
+    <section className={styles.hero}>
+      <div className={styles.hero_inner}>
+      <div className={styles.hero_text}>
+      <h1 className={styles.hero_title}>Call to action text</h1>
+      <p className={styles.hero_paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id tincidunt dapibus, magna libero sodales massa, sed aliquam nunc massa id nisl.</p>
+      </div>
+      <div className={styles.hero_numbers}>
+        <div className={styles.hero_numbers_block}>
+          <h3>200+</h3>
+          <p>Rented homes</p>
+        </div>
+        <div className={styles.hero_numbers_block}>
+          <h3>1000+</h3>
+          <p>Happy guests</p>
+        </div>
+        <div className={styles.hero_numbers_block}>
+          <h3>15+</h3>
+          <p>Years of experience</p>
+        </div>
+      </div>
+      <Link href="/apartments" className={styles.secondary_btn}>See apartments</Link>
+      </div>
+    </section>
+      </header> 
+      <main className={styles.main}></main>
     <Footer />
     
     </>
